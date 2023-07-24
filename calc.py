3a97e9eb39aeb0ba0e96b360e4f1835f5f522323
rushikesh kumbhakarn <rushikeshkumbhakarn47334@gmail.com>
2023-07-24 12:42:13 +0530

    Sub com

--- calc.py
+++ calc.py
@@ -4,11 +4,14 @@
 
 # SUB function defination
 
+def subtract(n1,n2):
+	print("The subtraction is: ",n1-n2)
 
 if __name__ == "__main__":
-	n1 = input("Enter num1")
-	n2 = input("Enter num2")
+	n1 = int(input("Enter num1"))
+	n2 = int(input("Enter num2"))
 
 	#ADD function call below
 
 	#SUB function call below
+	subtract(n1,n2)
